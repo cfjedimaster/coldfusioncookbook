c3b18edf7d9c629d87c25bb0a1b3b726f2c70557
Raymond Camden <raymondcamden@gmail.com>
2021-12-29 13:26:09 -0600

    rss

--- .eleventy.js
+++ .eleventy.js
@@ -1,17 +1,19 @@
+const pluginRss = require("@11ty/eleventy-plugin-rss");
+
 module.exports = function(eleventyConfig) {
+
+	eleventyConfig.addPlugin(pluginRss);
 
 	eleventyConfig.addPassthroughCopy('stylesheets/**');
 	eleventyConfig.addPassthroughCopy('images/**');
 	eleventyConfig.addPassthroughCopy('js/**');
 
-	/*
-	*/
 	eleventyConfig.addFilter("catLink", (cat,categories) => {
 		for(let c in categories) {
 			if(categories[c].title === cat) return categories[c].slug+".html";			
 		}
 		console.error('Bad category', JSON.stringify(cat));
-	})
+	});
 
 	/*
 	This is how I filter entries by a category. We are passed the URL safe category ("Dates-Times"), 
@@ -19,7 +21,6 @@ module.exports = function(eleventyConfig) {
 	then find entries where it's in the list
 	*/
 	eleventyConfig.addFilter("filterByCat", (entries,category,list) => {
-		console.log('get entries for ',category);
 		let nice = '';
 		for(let c in list) {
 			if(list[c].slug === category) nice = list[c].title
@@ -28,6 +29,13 @@ module.exports = function(eleventyConfig) {
 			let entriesCats = e.data.categories.split(',');
 			return entriesCats.includes(nice);
 		});
+	});
+
+	/* used for rss */
+	eleventyConfig.addFilter("fixDate", d => {
+		let newDate = new Date(d);
+		return newDate;
 	})
+
 
 };
